95c913775e6e68f99c007125f0915a97b22836c0
mrsaiello <35349573+mrsaiello@users.noreply.github.com>
2021-04-26 10:43:34 -0400

    Add files via upload

---
+++ chap55/javaHierarych.java
@@ -0,0 +1,20 @@
+class Object
+{
+}
+
+class Boolean extends Object
+{
+}
+
+class Character extends Object
+{
+}
+
+abstract class Component extends Object
+{
+}
+
+class String extends Object
+{
+}
+
